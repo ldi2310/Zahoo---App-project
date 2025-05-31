fa354668f4aa77d855bc2ee20be88031d43ededc
Hanyui Hanawa <hanyuihanawa@gmail.com>
2025-05-31 21:44:57 +0700

    Add files via upload

--- app/src/main/java/com/example/zahoo/ui/TopNavBar.kt
+++ app/src/main/java/com/example/zahoo/ui/TopNavBar.kt
@@ -1,23 +1,41 @@
 package com.example.zahoo.ui.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
+import androidx.navigation.NavHostController
 
 @Composable
-fun TopNavBar() {
+fun TopNavBar(navController: NavHostController) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
-        horizontalArrangement = Arrangement.Center,
+            .padding(vertical = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text("Zahoo", fontSize = 30.sp, color = Color.Blue)
+        Text(
+            text = "Zahoo",
+            fontSize = 40.sp,
+            color = Color.Blue,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+
+        Icon(
+            imageVector = Icons.Default.Search,
+            contentDescription = "Search",
+            modifier = Modifier
+                .padding(end = 16.dp)
+                .size(40.dp)
+                .clickable {
+                    navController.navigate("search_screen") // ✅ điều hướng
+                }
+        )
     }
 }
